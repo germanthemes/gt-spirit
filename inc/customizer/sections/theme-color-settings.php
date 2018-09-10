418cf1f2fb30b856d742d2a9052b82368a51b207
Thomas Weichselbaumer <kontakt@themecoder.de>
2018-09-10 20:26:33 +0200

    renamed Color Settings to Theme Colors

--- inc/customizer/sections/theme-color-settings.php
+++ inc/customizer/sections/theme-color-settings.php
@@ -1,8 +1,6 @@
 <?php
 /**
- * Color Settings
- *
- * Register Color Settings
+ * Theme Color Settings
  *
  * @package GT Spirit
  */
@@ -12,11 +10,11 @@
  *
  * @param object $wp_customize / Customizer Object.
  */
-function gt_spirit_customize_register_color_settings( $wp_customize ) {
+function gt_spirit_customize_register_theme_color_settings( $wp_customize ) {
 
 	// Add Section for Theme Colors.
-	$wp_customize->add_section( 'gt_spirit_section_colors', array(
-		'title'    => esc_html_x( 'Colors', 'Color Settings', 'gt-spirit' ),
+	$wp_customize->add_section( 'gt_spirit_section_theme_colors', array(
+		'title'    => esc_html_x( 'Theme Colors', 'theme colors', 'gt-spirit' ),
 		'priority' => 30,
 		'panel'    => 'gt_spirit_options_panel',
 	) );
@@ -33,8 +31,8 @@ function gt_spirit_customize_register_color_settings( $wp_customize ) {
 	) );
 	$wp_customize->add_control( new WP_Customize_Color_Control(
 		$wp_customize, 'gt_spirit_theme_options[link_color]', array(
-			'label'    => esc_html_x( 'Links & Buttons (primary)', 'Color Settings', 'gt-spirit' ),
-			'section'  => 'gt_spirit_section_colors',
+			'label'    => esc_html_x( 'Links & Buttons (primary)', 'theme colors', 'gt-spirit' ),
+			'section'  => 'gt_spirit_section_theme_colors',
 			'settings' => 'gt_spirit_theme_options[link_color]',
 			'priority' => 10,
 		)
@@ -49,8 +47,8 @@ function gt_spirit_customize_register_color_settings( $wp_customize ) {
 	) );
 	$wp_customize->add_control( new WP_Customize_Color_Control(
 		$wp_customize, 'gt_spirit_theme_options[link_hover_color]', array(
-			'label'    => esc_html_x( 'Links & Buttons (secondary)', 'Color Settings', 'gt-spirit' ),
-			'section'  => 'gt_spirit_section_colors',
+			'label'    => esc_html_x( 'Links & Buttons (secondary)', 'theme colors', 'gt-spirit' ),
+			'section'  => 'gt_spirit_section_theme_colors',
 			'settings' => 'gt_spirit_theme_options[link_hover_color]',
 			'priority' => 20,
 		)
@@ -65,8 +63,8 @@ function gt_spirit_customize_register_color_settings( $wp_customize ) {
 	) );
 	$wp_customize->add_control( new WP_Customize_Color_Control(
 		$wp_customize, 'gt_spirit_theme_options[header_color]', array(
-			'label'    => esc_html_x( 'Header', 'Color Settings', 'gt-spirit' ),
-			'section'  => 'gt_spirit_section_colors',
+			'label'    => esc_html_x( 'Header', 'theme colors', 'gt-spirit' ),
+			'section'  => 'gt_spirit_section_theme_colors',
 			'settings' => 'gt_spirit_theme_options[header_color]',
 			'priority' => 30,
 		)
@@ -81,8 +79,8 @@ function gt_spirit_customize_register_color_settings( $wp_customize ) {
 	) );
 	$wp_customize->add_control( new WP_Customize_Color_Control(
 		$wp_customize, 'gt_spirit_theme_options[navi_color]', array(
-			'label'    => esc_html_x( 'Navigation', 'Color Settings', 'gt-spirit' ),
-			'section'  => 'gt_spirit_section_colors',
+			'label'    => esc_html_x( 'Navigation', 'theme colors', 'gt-spirit' ),
+			'section'  => 'gt_spirit_section_theme_colors',
 			'settings' => 'gt_spirit_theme_options[navi_color]',
 			'priority' => 40,
 		)
@@ -97,8 +95,8 @@ function gt_spirit_customize_register_color_settings( $wp_customize ) {
 	) );
 	$wp_customize->add_control( new WP_Customize_Color_Control(
 		$wp_customize, 'gt_spirit_theme_options[title_color]', array(
-			'label'    => esc_html_x( 'Titles (primary)', 'Color Settings', 'gt-spirit' ),
-			'section'  => 'gt_spirit_section_colors',
+			'label'    => esc_html_x( 'Titles (primary)', 'theme colors', 'gt-spirit' ),
+			'section'  => 'gt_spirit_section_theme_colors',
 			'settings' => 'gt_spirit_theme_options[title_color]',
 			'priority' => 50,
 		)
@@ -113,8 +111,8 @@ function gt_spirit_customize_register_color_settings( $wp_customize ) {
 	) );
 	$wp_customize->add_control( new WP_Customize_Color_Control(
 		$wp_customize, 'gt_spirit_theme_options[title_hover_color]', array(
-			'label'    => esc_html_x( 'Titles (secondary)', 'Color Settings', 'gt-spirit' ),
-			'section'  => 'gt_spirit_section_colors',
+			'label'    => esc_html_x( 'Titles (secondary)', 'theme colors', 'gt-spirit' ),
+			'section'  => 'gt_spirit_section_theme_colors',
 			'settings' => 'gt_spirit_theme_options[title_hover_color]',
 			'priority' => 60,
 		)
@@ -129,11 +127,11 @@ function gt_spirit_customize_register_color_settings( $wp_customize ) {
 	) );
 	$wp_customize->add_control( new WP_Customize_Color_Control(
 		$wp_customize, 'gt_spirit_theme_options[footer_color]', array(
-			'label'    => esc_html_x( 'Footer', 'Color Settings', 'gt-spirit' ),
-			'section'  => 'gt_spirit_section_colors',
+			'label'    => esc_html_x( 'Footer', 'theme colors', 'gt-spirit' ),
+			'section'  => 'gt_spirit_section_theme_colors',
 			'settings' => 'gt_spirit_theme_options[footer_color]',
 			'priority' => 70,
 		)
 	) );
 }
-add_action( 'customize_register', 'gt_spirit_customize_register_color_settings' );
+add_action( 'customize_register', 'gt_spirit_customize_register_theme_color_settings' );
