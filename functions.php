f3c80e08d06b3e169a4fd2e6a84152b48b7181cc
Thomas Weichselbaumer <kontakt@themecoder.de>
2019-12-11 19:39:17 +0100

    minified javascript files

--- functions.php
+++ functions.php
@@ -104,7 +104,7 @@ function gt_spirit_scripts() {
 
 	// Register and enqueue navigation.js.
 	if ( has_nav_menu( 'primary' ) ) {
-		wp_enqueue_script( 'gt-spirit-navigation', get_theme_file_uri( '/assets/js/navigation.js' ), array( 'jquery' ), '1.0', true );
+		wp_enqueue_script( 'gt-spirit-navigation', get_theme_file_uri( '/assets/js/navigation.min.js' ), array( 'jquery' ), '1.0', true );
 		$gt_spirit_l10n = array(
 			'expand'   => esc_html__( 'Expand child menu', 'gt-spirit' ),
 			'collapse' => esc_html__( 'Collapse child menu', 'gt-spirit' ),
