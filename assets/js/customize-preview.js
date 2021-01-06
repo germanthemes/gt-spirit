0cc938240d2bdfeef8e2e3f8ef0a987c47a2b610
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-01-06 21:42:16 +0100

    updated typography options to use WPTT Webfont loader

--- assets/js/customize-preview.js
+++ assets/js/customize-preview.js
@@ -379,20 +379,8 @@
 	}
 
 	function loadCustomFont( font, type ) {
-		if (typeof gtSpiritFontSettings === 'undefined') {
-			return;
-		}
-
-		var defaultFonts = ['Arial', 'Arial Black', 'Courier New', 'Georgia', 'Helvetica',
-			'Impact', 'Palatino, Palatino Linotype', 'SystemFontStack', 'Tahoma',
-			'Trebuchet MS, Trebuchet', 'Times New Roman, Times', 'Verdana'];
-
-		if( defaultFonts.includes( font ) ) {
-			return;
-		}
-
-		var fontFile = font.split( " " ).join( "-" ).toLowerCase();
-		var fontFileURL = gtSpiritFontSettings.pluginURL + fontFile + ".css";
+		var fontFile = font.split( " " ).join( "+" );
+		var fontFileURL = "https://fonts.googleapis.com/css?family=" + fontFile + ":400,700";
 
 		var fontStylesheet = "<link id='gt-spirit-custom-" + type + "' href='" + fontFileURL + "' rel='stylesheet' type='text/css'>";
 		var checkLink = $( "head" ).find( "#gt-spirit-custom-" + type ).length;
